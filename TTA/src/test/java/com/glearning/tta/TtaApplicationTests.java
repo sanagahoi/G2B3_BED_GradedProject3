cbe7d14ea913bee918c15c42a0acb46b48347bd4
sanagahoi <111426480+sanagahoi@users.noreply.github.com>
2023-01-22 22:34:42 +0530

    Add files via upload

---
+++ TTA/src/test/java/com/glearning/tta/TtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glearning.tta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
